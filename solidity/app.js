a5ccb63718e9ad83aece196e279735fc9c483973
Sami Mäkelä <mrsmkl@gmail.com>
2017-09-11 16:47:03 +0300

    trying to get posting tasks working

--- solidity/app.js
+++ solidity/app.js
@@ -11,16 +11,19 @@ var ipfsAPI = require('ipfs-api')
 // connect to ipfs daemon API server
 var ipfs = ipfsAPI('programming-progress.com', '5001', {protocol: 'http'})
 
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
+web3.setProvider(new web3.providers.HttpProvider('http://programming-progress.com:8545'))
 
-var base = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+// var base = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+var base = "0x9acbcf2d9bd157999ae5541c446f8d6962da1d4d"
 
 var code = fs.readFileSync("contracts/Tasks.bin")
 var abi = JSON.parse(fs.readFileSync("contracts/Tasks.abi"))
 
 var send_opt = {from:base, gas: 4000000}
 
-var contract = new web3.eth.Contract(abi, "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab")
+// var contract = new web3.eth.Contract(abi, "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab")
+var contractABI = web3.eth.contract(abi)
+var contract = contractABI.at("0xEDc02d969AF38367B49DCB36b5A2ef7abb8A7fA4")
 
 io.on("connection", function(socket) {
     console.log("Got client")
@@ -49,11 +52,61 @@ io.on("connection", function(socket) {
                     }
                     console.log('stdout', stdout)
                     // It should give the initial state hash, post it to contract
-                    // contract.methods.add(JSON.parse(stdout), res.hash).send(send_opt)
-                    contract.methods.add(JSON.parse(stdout), res[0].hash).send(send_opt)
+                    contract.add(JSON.parse(stdout), res[0].hash, send_opt, function (err, tr) {
+                        if (err) console.log(err)
+                        else {
+                            console.log("Success", tr)
+                            io.emit("task_success", tr)
+                        }
+                    })
                 })
             })
         })
+    })
+})
+
+function solveTask(obj) {
+    var filename = obj.filehash + ".wast"
+    // store into filesystem
+    fs.writeFile(filename, obj.file, function () {
+        execFile('../interpreter/wasm', ["-m", "-init", "0", filename], (error, stdout, stderr) => {
+            if (error) {
+                console.error('stderr', stderr)
+                return
+            }
+            var inithash = JSON.parse(stdout)
+            if (inithash == obj.hash) {
+                console.log("Initial hash matches")
+            }
+            else console.log("Initial hash was wrong")
+        })
+    })
+}
+
+// var ev = contract.Posted({}, {fromBlock:0, toBlock:"latest"}, function () { console.log("here") }).get(function (err, res) { console.log(res) })
+contract.Posted("latest").watch(function (err, ev) {
+    if (err) console.log(err)
+    io.emit("posted", {giver: ev.args.giver, hash: ev.args.hash, file:ev.args.file, id:ev.args.id.toString()})
+    // download file from IPFS
+    ipfs.get(ev.args.file, function (err, stream) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        stream.on('data', (file) => {
+            file.content.on("data", function (chunk) {
+                console.log("chnnk")
+                chunks.push(chunk);
+            })
+
+            // Send the buffer or you can put it into a var
+            file.content.on("end", function () {
+                var filestr = Buffer.concat(chunks).toString()
+                solveTask({giver: ev.args.giver, hash: ev.args.hash, file:filestr, filehash:ev.args.file, id:ev.args.id.toString()})
+            })
+        })
+        var chunks = []
+
     })
 })
 
--- solidity/deploy-tasks.js
+++ solidity/deploy-tasks.js
@@ -3,9 +3,9 @@ var fs = require("fs")
 var Web3 = require('web3')
 var web3 = new Web3()
 
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
+web3.setProvider(new web3.providers.HttpProvider('http://programming-progress.com:8545'))
 
-var base = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+var base = "0x9acbcf2d9bd157999ae5541c446f8d6962da1d4d"
 
 var code = fs.readFileSync("contracts/Tasks.bin")
 var abi = JSON.parse(fs.readFileSync("contracts/Tasks.abi"))
@@ -14,7 +14,7 @@ var send_opt = {from:base, gas: 4000000}
 
 var sol_testContract = new web3.eth.Contract(abi)
 
-sol_testContract.deploy({data:code}).send(send_opt).then(function (contract) {
+sol_testContract.deploy({data:"0x"+code}).send(send_opt).then(function (contract) {
   console.log('Contract mined! address: ' + contract.options.address)
 })
 
