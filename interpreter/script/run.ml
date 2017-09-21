b5cbcc8b75300f52ccec7e4e66d66cd2fc244a9b
Sami Mäkelä <mrsmkl@gmail.com>
2017-09-21 09:20:07 +0300

    printing result when the execution ends with error

--- interpreter/script/run.ml
+++ interpreter/script/run.ml
@@ -351,9 +351,13 @@ let run_test inst mdle func vs =
 (*    trace (Printexc.to_string a);
     Printexc.print_backtrace stderr; *)
     values_from_arr vm.stack 0 vm.stack_ptr
+   | a -> (* Print error result *)
+    if !task_number = !Flags.case + 1 && !Flags.result then Printf.printf "{\"result\": \"0x0\", \"steps\": %i}\n" (!last_step + 1);
+   ( match a with
    | Numeric_error.IntegerOverflow -> raise (Eval.Trap (no_region, "integer overflow"))
    | Numeric_error.InvalidConversionToInteger -> raise (Eval.Trap (no_region, "invalid conversion to integer"))
    | Numeric_error.IntegerDivideByZero -> raise (Eval.Trap (no_region, "integer divide by zero"))
+   | a -> raise a )
 
 let run_test_micro inst mdle func vs =
   let open Mrun in
