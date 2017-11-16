5955efc3f68375e22efaf9b6745bebb79c3e7119
Sami Mäkelä <mrsmkl@gmail.com>
2017-11-16 19:19:19 +0200

    implemented converting wasm files with floating points to deterministic format

--- interpreter/host/env.ml
+++ interpreter/host/env.ml
@@ -50,6 +50,7 @@ let lookup name t =
   | "memoryBase", ExternalGlobalType t -> ExternalGlobal (I32 0l)
   | "tableBase", ExternalGlobalType t -> ExternalGlobal (I32 0l)
   | "___dso_handle", ExternalGlobalType t -> ExternalGlobal (I32 0l)
+  | "__dso_handle", ExternalGlobalType t -> ExternalGlobal (I32 0l)
   | _, ExternalFuncType t -> ExternalFunc (HostFunc (t, abort))
   | "memory", ExternalMemoryType (MemoryType {min;max}) -> ExternalMemory (Memory.create {min;max})
   | "table", ExternalTableType (TableType ({min;max}, t)) -> ExternalTable (Table.create t {min;max})
