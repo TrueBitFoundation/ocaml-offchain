d939e849bb76f0eef6ff188fbc4b53def1f9b72b
Sami Mäkelä <mrsmkl@gmail.com>
2017-11-29 11:02:28 +0200

    handling constants and globals

--- interpreter/merkle/intfloat.ml
+++ interpreter/merkle/intfloat.ml
@@ -35,6 +35,8 @@ let convert_type t = do_it t convert_type'
 let convert_ftype ft =
   do_it ft (function FuncType (l1, l2) -> FuncType (List.map convert_type' l1, List.map convert_type' l2))
 
+let convert_gtype = function GlobalType (t,mut) -> GlobalType (convert_type' t, mut)
+
 let rec convert_inst' = function
  | Block (ty, lst) -> Block (List.map convert_type' ty, List.map convert_inst lst) 
  | Loop (ty, lst) -> Loop (List.map convert_type' ty, List.map convert_inst lst)
@@ -65,33 +67,34 @@ let find_function m (name:string) =
      else find tl in
   {it=find m.it.exports; at=no_region}
 
-(*
-
-| Test of testop                    (* numeric test *)
-| Compare of relop                  (* numeric comparison *)
-| Unary of unop                     (* unary numeric operator *)
-| Binary of binop                   (* binary numeric operator *)
-| Convert of cvtop                  (* conversion *)
-
-*)
-
 (* add ops ... *)
-
 
 let convert_float m =
   let rec convert_op' = function
-   | Block (ty, lst) -> [Block (ty, List.flatten (List.map convert_op lst))]
-   | Loop (ty, lst) -> [Loop (ty, List.flatten (List.map convert_op lst))]
-   | If (ty, texp, fexp) -> [If (ty, List.flatten (List.map convert_op texp), List.flatten (List.map convert_op fexp))]
+   | Block (ty, lst) -> [Block (ty, convert_body lst)]
+   | Loop (ty, lst) -> [Loop (ty, convert_body lst)]
+   | If (ty, texp, fexp) -> [If (ty, convert_body texp, convert_body fexp)]
    | Binary (F32 F32Op.Add) -> [Call (find_function m "f32_add")]
    | Binary (F32 F32Op.Div) -> [Call (find_function m "f32_div")]
+   | Binary (F32 F32Op.Mul) -> [Call (find_function m "f32_mul")]
+   | Binary (F32 F32Op.Sub) -> [Call (find_function m "f32_sub")]
    | Convert (F32 F32Op.ConvertSI32) -> [Call (find_function m "i32_to_f32")]
+   | Convert (F32 F32Op.ConvertUI32) -> [Call (find_function m "ui32_to_f32")]
+   | Convert (F64 F64Op.ConvertSI32) -> [Call (find_function m "i32_to_f64")]
+   | Convert (F64 F64Op.ConvertUI32) -> [Call (find_function m "ui32_to_f64")]
+   | Convert (F32 F32Op.ConvertSI64) -> [Call (find_function m "i64_to_f32")]
+   | Convert (F32 F32Op.ConvertUI64) -> [Call (find_function m "ui64_to_f32")]
+   | Convert (F64 F64Op.ConvertSI64) -> [Call (find_function m "i64_to_f64")]
+   | Convert (F64 F64Op.ConvertUI64) -> [Call (find_function m "ui64_to_f64")]
    | Convert (I32 I32Op.TruncSF32) -> [Const (elem (I32 (Int32.of_int 0))); Const (elem (I32 (Int32.of_int 0))); Call (find_function m "f32_to_i32")]
+   | Const {it=F32 f; _} -> [Const (elem (I32 (F32.to_bits f)))]
+   | Const {it=F64 f; _} -> [Const (elem (I64 (F64.to_bits f)))]
    | a -> [a]
-  and convert_op x = List.map elem (convert_op' x.it) in
-  let convert_func f = do_it f (fun f -> {f with body=List.flatten (List.map convert_op f.body)}) in
-  (* also constants have to be converted *)
-  do_it m (fun m -> {m with funcs=List.map convert_func m.funcs})
+  and convert_op x = List.map elem (convert_op' x.it)
+  and convert_body lst = List.flatten (List.map convert_op lst) in
+  let convert_func f = do_it f (fun f -> {f with body=convert_body f.body}) in
+  let convert_global g = do_it g (fun g -> {value=do_it g.value convert_body; gtype=convert_gtype g.gtype}) in
+  do_it m (fun m -> {m with funcs=List.map convert_func m.funcs; globals=List.map convert_global m.globals})
 
 let process a b =
   convert_float (convert_types (merge a b))
