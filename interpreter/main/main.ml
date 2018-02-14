e529ace8ac838f8c5aca06266c8f280336345a94
Sami Mäkelä <mrsmkl@gmail.com>
2018-02-14 14:46:23 -0800

    add custom judge from command line

--- interpreter/main/main.ml
+++ interpreter/main/main.ml
@@ -2,6 +2,14 @@
 let name = "wasm"
 let version = "1.0"
 
+let add_custom_judge str =
+  match String.split_on_char ',' str with
+  | [num; symbol; command] ->
+    let num = int_of_string num in
+    Hashtbl.add Merkle.custom_calls symbol num;
+    Hashtbl.add Mrun.custom_command num command
+  | _ -> prerr_endline "bad format for custom judge"
+
 let configure () =
   Import.register (Utf8.decode "spectest") Spectest.lookup;
   Import.register (Utf8.decode "input") Input.lookup;
@@ -98,6 +106,8 @@ let argspec = Arg.align
   "-sbrk-offset", Arg.Int (fun n -> Flags.sbrk_offset := Int32.of_int n), " memory offset used by sbrk";
   "-output-step", Arg.Int (fun x -> Flags.output_file_at := x), " for which step the file will be output";
   "-output-file", Arg.Int (fun x -> Flags.output_file_number := x), " which file will be output at the given step";
+  
+  "-custom", Arg.String add_custom_judge, " Add custom judge. Format: num,symbol,judge";
 ]
 
 let () =
