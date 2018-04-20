07849995d01b3dece3bba67b151fab84efce4ecd
Sami Mäkelä <mrsmkl@gmail.com>
2018-04-20 13:16:19 +0300

    adding proofs for out of bounds access

--- interpreter/script/run.ml
+++ interpreter/script/run.ml
@@ -454,11 +454,11 @@ let run_test inst mdle func vs =
            if i = !Flags.insert_error && !task_number - 1 = !Flags.case then Mproof.micro_step_proofs_with_error vm
            else Mproof.micro_step_proofs vm in
          Mproof.check_proof proof
-      end else Mrun.vm_step vm;
+      end else ( test_errors vm ; Mrun.vm_step vm );
       ( if i = !Flags.insert_error && !task_number - 1 = !Flags.case then Mrun.set_input_name vm 0 10 (Values.I32 1l) ); 
       vm.step <- vm.step + 1;
       (* if i mod 10000000 = 0 then prerr_endline "."; *)
-      test_errors vm
+      (* test_errors vm *)
     done;
     raise (Failure "takes too long")
   end
